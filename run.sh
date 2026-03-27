952260f9f6b94006d0cdbb56ed77bf720c931f9a
klausibaer <lukasklemend@googlemail.com>
2026-03-27 13:32:50 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,5 @@
-#!/usr/bin/with-contenv bashio
-
-export CLAUDE_API_KEY=$(bashio::config 'claude_api_key')
-export GMAIL_CLIENT_ID=$(bashio::config 'gmail_client_id')
-export GMAIL_CLIENT_SECRET=$(bashio::config 'gmail_client_secret')
-export GMAIL_REFRESH_TOKEN=$(bashio::config 'gmail_refresh_token')
-export GCAL_CLIENT_ID=$(bashio::config 'gcal_client_id')
-export GCAL_CLIENT_SECRET=$(bashio::config 'gcal_client_secret')
-export GCAL_REFRESH_TOKEN=$(bashio::config 'gcal_refresh_token')
-
-bashio::log.info "Arztsuche Outreach starting..."
-python3 /app/server.py
+#!/bin/bash
+set -e
+echo "[INFO] Starting Arztsuche Outreach on port 5055..."
+cd /app
+exec python3 server.py
